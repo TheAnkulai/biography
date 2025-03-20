4d3ca72cc416c9df9a6832a17155f3ee34ce2d37
TheAnkulai <s79528662968@gmail.com>
2025-03-20 15:08:42 +0300

    sduvsifuvsvbosfv

---
+++ script/script.js
@@ -0,0 +1,6 @@
+function openTab(tabId) {
+    let tabs = document.querySelectorAll('.tab');
+    tabs.forEach(tab => tab.classList.remove('active'));
+
+    document.getElementById(tabId).classList.add('active');
+}
